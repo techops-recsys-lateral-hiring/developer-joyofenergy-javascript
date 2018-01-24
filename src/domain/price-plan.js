174a0bab745dba4c2fa0ccc1eaccae903bd5b1f4
damned <writetodan@yahoo.com>
2018-01-24 17:43:38 +0000

    Dan: peak time multipliers

--- src/domain/price-plan.js
+++ src/domain/price-plan.js
@@ -8,6 +8,25 @@ class PricePlan {
         this.unitRate = unitRate        
     }
 
+    getPrice(dateTime) {
+        return this.unitRate;
+    }
+}
+
+PricePlan.DayOfWeek = Object.freeze({
+    Monday:1, 
+    Tuesday:2, 
+    Wednesday:3, 
+    Thuesday:4, 
+    Friday:5, 
+    Saturday:6, 
+    Sunday:7, 
+})
+
+PricePlan.PeakTimeMultiplier = class {
+    constructor(dayOfWeek, multiplier) {
+
+    }
 }
 
 module.exports = PricePlan
