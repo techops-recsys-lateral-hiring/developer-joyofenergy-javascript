58777c932ea0c8a4c0b089068f7dee730c841602
damned <writetodan@yahoo.com>
2018-01-31 10:46:16 +0000

    Configure meter ids / price plans mappings

--- src/service/account-service.js
+++ src/service/account-service.js
@@ -3,11 +3,17 @@
 class AccountService {
 
     constructor() {
-        
+        this.planIdsByMeter = {
+            "meter-0": "price-plan-0",
+            "meter-1": "price-plan-1",
+            "meter-2": "price-plan-0",
+            "meter-3": "price-plan-2",
+            "meter-4": "price-plan-1",
+        }
     }
 
     getPricePlan(smartMeterId) {
-        return "price-plan-1"
+        return this.planIdsByMeter[smartMeterId]
     }
 }
 
