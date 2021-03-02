8e394bd3bf6eb871cc89d3128f5665e200b58efe
Alper Köse <alperkose@gmail.com>
2021-03-02 12:08:09 +0100

    Fix a bug for /recommend and /compare-all endpoints

--- src/price-plans/price-plans-controller.js
+++ src/price-plans/price-plans-controller.js
@@ -3,11 +3,21 @@ const { usageForAllPricePlans } = require("../usage/usage");
 
 const recommend = (getReadings, req) => {
     const meter = req.params.smartMeterId;
-    return usageForAllPricePlans(pricePlans, getReadings(meter));
+    const pricePlanComparisons = usageForAllPricePlans(pricePlans, getReadings(meter)).sort((a, b) => extractCost(a) - extractCost(b))
+    if("limit" in req.params) {
+        return pricePlanComparisons.slice(0, req.params.limit);
+    }
+    return pricePlanComparisons;
 };
 
+const extractCost = (cost) => {
+    const [, value] = Object.entries(cost).find( ([key]) => key in pricePlans)
+    return value
+}
+
 const compare = (getData, req) => {
-    const pricePlanComparisons = recommend(getData, req);
+    const meter = req.params.smartMeterId;
+    const pricePlanComparisons = usageForAllPricePlans(pricePlans, getData(meter));;
     return {
         smartMeterId: req.params.smartMeterId,
         pricePlanComparisons,
