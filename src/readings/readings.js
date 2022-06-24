a86db8c944a7b83e706855addd0bff568ff1a445
Akkapon Somjai <asomjai@th.palo-it.com>
2022-06-24 08:13:36 +0700

    Fix bug when the user tries to add a new meter reccord

--- src/readings/readings.js
+++ src/readings/readings.js
@@ -1,10 +1,10 @@
 const readings = (data) => ({
-    getReadings: (meterId) => data[meterId] || [],
-    setReadings: (meterId, readings) => {
-        const currentReadings = data[meterId];
-        data[meterId] = [...currentReadings, ...readings];
-        return data[meterId];
-    },
+  getReadings: (meterId) => data[meterId] || [],
+  setReadings: (meterId, readings) => {
+    const currentReadings = data.hasOwnProperty(meterId) ? data[meterId] : [];
+    data[meterId] = [...currentReadings, ...readings];
+    return data[meterId];
+  },
 });
 
 module.exports = { readings };
